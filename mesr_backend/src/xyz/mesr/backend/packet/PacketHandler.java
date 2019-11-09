acf1004c68b70722f57b1ad9dfc22de6daf7f781
Godly <Godly@DESKTOP-O0TT5RQ.local>
2019-11-09 18:04:42 +0100

    Starting with packet system

---
+++ mesr_backend/src/xyz/mesr/backend/packet/PacketHandler.java
@@ -0,0 +1,13 @@
+package xyz.mesr.backend.packet;
+
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class PacketHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+}
