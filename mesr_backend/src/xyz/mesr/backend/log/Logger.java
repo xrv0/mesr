2f83ff1a7ccd0660bea92138e3d26c569c35d06f
xrv0 <33809091+xrv0@users.noreply.github.com>
2019-11-09 21:14:48 +0100

    Minor changes to Logger
    Added better javadoc and warning, error and information methods now return the printed message

--- mesr_backend/src/xyz/mesr/backend/log/Logger.java
+++ mesr_backend/src/xyz/mesr/backend/log/Logger.java
@@ -10,7 +10,15 @@ import java.util.ArrayList;
  * Logger class for managing all warnings, errors, and information
  */
 public class Logger {
+    /**
+     * Default Logger instance
+     */
     public static final Logger INSTANCE = new Logger();
+
+    private static final String WARNING_PREFIX = "[Warning] ";
+    private static final String ERROR_PREFIX = "[Warning] ";
+    private static final String INFORMATION_PREFIX = "[Warning] ";
+
     private ArrayList<String> messages;
 
     public Logger() {
@@ -18,44 +26,56 @@ public class Logger {
     }
 
     /**
-     * Prints out a error message and stores it temporarily
+     * Prints out a error message
+     * @param message Provides more information
+     * @return the printed message
      */
-    public void error(String message) {
-        this.messages.add(LocalDateTime.now().toString() + " : [Message]" + message);
+    public String error(String message) {
+        var output = LocalDateTime.now().toString() + " : " + ERROR_PREFIX + message;
+        System.err.println(ERROR_PREFIX + message);
+        this.messages.add(output);
+        return output;
+    }
+
+    /**
+     * Prints out an information message
+     * @param message Provides more information
+     * @return the printed message
+     */
+    public String information(String message) {
+        var output = LocalDateTime.now().toString() + " : " + INFORMATION_PREFIX + message;
+        this.messages.add(output);
         System.err.println("[Error] " + message);
+        return output;
     }
 
     /**
-     * Prints out a information message and stores it temporarily
+     * Prints out a warning message
+     * @param message Provides more information
+     * @return the printed message
      */
-    public void information(String message) {
-        this.messages.add(LocalDateTime.now().toString() + " : [Information] " + message);
-        System.out.println("[Information] " + message);
+    public String warning(String message) {
+        var output = LocalDateTime.now().toString() + " : " + WARNING_PREFIX + message;
+        this.messages.add(output);
+        System.err.println(WARNING_PREFIX + message);
+        return output;
     }
 
     /**
-     * Prints out a warning message and stores it temporarily
+     * Saves all messages from the current session to file
+     * @param log File to save the information to
+     * @return Was the save successful
      */
-    public void warning(String message) {
-        this.messages.add(LocalDateTime.now().toString() + " : [Warning] " + message);
-        System.out.println("[Warning] " + message);
-    }
-
-    /**
-     * Saves all logs for the current session to a file
-     * THE FILE WILL BE OVERWRITTEN
-     */
-    public void saveToFile(File output) {
+    public boolean save(File log) {
         try {
-            FileWriter logWriter = new FileWriter(output);
+            FileWriter logWriter = new FileWriter(log);
             for(String message : messages) {
-                logWriter.append(message + "\n");
+                logWriter.append(message);
             }
-            logWriter.flush();
-            logWriter.close();
+            return true;
         } catch (IOException e) {
-            e.printStackTrace();
+            this.error("There was an error while saving the log to file! [" + e.getMessage() + "]");
+            return false;
         }
-
     }
 }
