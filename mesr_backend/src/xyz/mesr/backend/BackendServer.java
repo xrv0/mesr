6da6168ac3177be9205227e117bc2ad50005fe6d
venum <mgautam@protonmail.com>
2019-11-13 20:35:39 +0100

    Added MariaDB connection

--- mesr_backend/src/xyz/mesr/backend/BackendServer.java
+++ mesr_backend/src/xyz/mesr/backend/BackendServer.java
@@ -4,6 +4,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.mariadb.jdbc.MariaDbConnection;
 import xyz.mesr.backend.conf.Configuration;
 import xyz.mesr.backend.conf.ConfigurationValue;
 import xyz.mesr.backend.log.Logger;
@@ -12,6 +13,8 @@ import xyz.mesr.backend.packet.logging.PacketLoggingHandler;
 import xyz.mesr.backend.user.User;
 
 import java.io.File;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
@@ -21,18 +24,41 @@ public class BackendServer extends Thread{
     private final Configuration configuration;
     private final ArrayList<User> loadedUsers;
     private ServerBootstrap serverBootstrap;
+    private MariaDbConnection mariaDbConnection;
 
-    @ConfigurationValue(name = "server.port")
+    @ConfigurationValue(name = "server.port", description = "Port to open the mesr server connection on")
     private int port = 4455;
     @ConfigurationValue(name = "server.name", description = "This name is not necessarily unique and showed to users in the server list")
     private String name = "mesr standard messaging server";
     @ConfigurationValue(name = "max_users", description = "Maximal amount of concurrent users")
     private int maxUsers;
+    @ConfigurationValue(name = "mariadb.hostname", description = "MariaDB hostname in IP or domain format")
+    private String mariadbHostname = "127.0.0.1";
+    @ConfigurationValue(name = "mariadb.port", description = "MariaDB Database port")
+    private short mariadbPort = 3306;
+    @ConfigurationValue(name = "mariadb.username", description = "MariaDB username")
+    private String mariadbUsername = "root";
+    @ConfigurationValue(name = "mariadb.port", description = "MariaDB password")
+    private String mariadbPassword = "password123";
 
     public BackendServer() {
         this.loadedUsers = new ArrayList<>();
-        this.configuration = new Configuration("config.properties");
-        this.configuration.saveValues(this);
+        this.configuration = new Configuration(new File("config.properties"), "mesr server configuration file");
+        this.configuration.setObjectValues(this);
+        this.connectMariaDB();
+    }
+
+    /**
+     * Connects to the MariaDB database
+     */
+    private void connectMariaDB() {
+        Logger.INSTANCE.information("Connecting to MariaDB database");
+        try {
+            this.mariaDbConnection = (MariaDbConnection) DriverManager.getConnection("jdbc:mariadb://" + mariadbHostname + ":" + mariadbPort, mariadbUsername, mariadbPassword);
+            Logger.INSTANCE.information("Successfully connected to the database");
+        } catch (SQLException e) {
+            Logger.INSTANCE.error("There was an error when connecting to the database", e);
+        }
     }
 
     /**
